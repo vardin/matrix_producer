a651c1d95309ad1abe93931d40004c4498be2c6a
Starcom <Starcom@DESKTOP-LQADJU0>
2017-05-18 20:53:57 +0900

    back_up thread

--- src/main/java/matrix_producer/matrix_producer.java
+++ src/main/java/matrix_producer/matrix_producer.java
@@ -6,16 +6,15 @@ import java.util.Properties;
 import java.util.Random;
 import java.util.concurrent.Future;
 
-import org.apache.kafka.clients.producer.*;  
+import org.apache.kafka.clients.producer.*;
 import org.apache.kafka.common.*;
 
 import org.opencv.core.Core;
 import org.opencv.core.Size;
-import org.opencv.core.Mat;
+import org.opencv.core.Mat; 
 import org.opencv.core.MatOfByte;
 import org.opencv.highgui.VideoCapture;
 import org.opencv.highgui.Highgui;
-
 
 public class matrix_producer implements Runnable {
 	private String Broker_URI;
@@ -23,6 +22,7 @@ public class matrix_producer implements Runnable {
 	private String cameraUrl;
 	private int count;
 	private Producer<String, byte[]> producer;
+	
 
 	// new Size(640, 480), 16
 
@@ -30,8 +30,9 @@ public class matrix_producer implements Runnable {
 
 		Broker_URI = "163.152.174.73:9092";
 		Kafka_Topic = "supercom";
-//		cameraUrl = "2";
-		count =0;
+		
+		// cameraUrl = "2";
+		count = 0;
 	}
 
 	public void run()
@@ -41,99 +42,94 @@ public class matrix_producer implements Runnable {
 		boolean isOpen = false;
 
 		VideoCapture cap = null;
-		int ElementsSize=529*529;
+		int ElementsSize = 529 * 529;
 
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 
 		int count = 0;
-		while (!isOpen) {
-			cap = new VideoCapture(0);
-			isOpen = cap.open(0);
-			System.out.println("Try to open times: " + ++count);
-		}
 
-		if (!isOpen) {
-			System.out.println("not open the stream!");
-			return;
-		}
+		/*
+		 * while (!isOpen) { cap = new VideoCapture(0); isOpen = cap.open(0);
+		 * System.out.println("Try to open times: " + ++count); }
+		 * 
+		 * if (!isOpen) { System.out.println("not open the stream!"); return; }
+		 * 
+		 */
 
-		Mat frame = new Mat();
-		
-	
+		// Mat frame = new Mat();
+
 		Properties props = new Properties();
 
 		props.put("bootstrap.servers", Broker_URI);
-	//	props.put("metadata.broker.list", Broker_URI);
+		// props.put("metadata.broker.list", Broker_URI);
 		props.put("acks", "all");
 		props.put("client.id", "super-group");
-		
+
 		props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 		props.put("value.serializer", "org.apache.kafka.common.serialization.ByteArraySerializer");
 		// props.put("serializer.class", "kafka.serializer.StringEncoder");
-		
-			  
-        producer = new KafkaProducer<String, byte[]>(props);  
-       
-		  Future<RecordMetadata> a = null;  
-		  
-	      long frameCount = 0;  
-		  
+
+		producer = new KafkaProducer<String, byte[]>(props);
+
+		Future<RecordMetadata> a = null;
+
+		long frameCount = 0;
 
 		while (true) {
 
-			cap.read(frame);
+			// cap.read(frame);
 
-	//		byte[] frameArray = new byte[((int) frame.total() * frame.channels())];
-	//		frame.get(0, 0, frameArray);
-			
+			// byte[] frameArray = new byte[((int) frame.total() *
+			// frame.channels())];
+			// frame.get(0, 0, frameArray);
+
 			byte[] byteArray = new byte[ElementsSize];
-			
-			for(int i=0;i<ElementsSize;i++)
-			{
-				byteArray[i] =(byte)(ElementsSize%128);
+
+			for (int i = 0; i < ElementsSize; i++) {
+				byteArray[i] = (byte) (ElementsSize % 128);
 			}
-			
+
 			/*
-			System.out.println("FrameSize:" + frameArray.length);
-			System.out.println("Mat:height " + frame.height());
-			System.out.println("Mat: width " + frame.width());
-			System.out.println("channels: " + frame.channels());
-			System.out.println("type: " + frame.type());
+			 * System.out.println("FrameSize:" + frameArray.length);
+			 * System.out.println("Mat:height " + frame.height());
+			 * System.out.println("Mat: width " + frame.width());
+			 * System.out.println("channels: " + frame.channels());
+			 * System.out.println("type: " + frame.type());
+			 * 
+			 * // KeyedMessage<String, byte[]> message = new
+			 * KeyedMessage<String, // byte[]>(Kafka_Topic, frameArray);
+			 * //KeyedMessage<String, byte[]> message = new KeyedMessage<String,
+			 * byte[]>(Broker_URI, frameArray);
+			 *  
+			 */
+			ProducerRecord<String, byte[]> producerRecord = new ProducerRecord<String, byte[]>(Kafka_Topic,
+					Long.toString(frameCount), byteArray);
 
-			// KeyedMessage<String, byte[]> message = new KeyedMessage<String,
-			// byte[]>(Kafka_Topic, frameArray);
-			//KeyedMessage<String, byte[]> message = new KeyedMessage<String, byte[]>(Broker_URI, frameArray);
-	
-			*/
-			ProducerRecord<String, byte[]> producerRecord = new ProducerRecord<String, byte[]>(Kafka_Topic,Long.toString(frameCount),byteArray);
+			a = producer.send(producerRecord);
 
-            a = producer.send(producerRecord);
-                           
-	          count++;
-            System.out.println("Send one frame");  
+			count++;
+			System.out.println("Send one frame");
 
-                      
 			try {
 				Thread.sleep(100);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-            
-                        
+
 		}
 	}
 
 	public static void main(String[] args) throws UnsupportedEncodingException {
 
 		matrix_producer Distributor = new matrix_producer();
-		
+
 		// distributor.Init(URLDecoder.decode(args[0], "UTF-8"), args[1],
 		// args[2]);
 		// distributor.Init(args[0], args[1], args[2]);
-			
+
 		Thread producerProcess = new Thread(Distributor);
-		
+
 		producerProcess.start();
 	}
 
